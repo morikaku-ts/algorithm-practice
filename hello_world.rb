7f17669bdca2e056f02c0ceb25a0600439b3ce36
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-01-18 15:54:08 +0900

    初めてのプログラミング実践
    - RubyでHello Worldを表示するプログラム
    - コメントの記載方法の確認

---
+++ hello_world.rb
@@ -0,0 +1,21 @@
+print "Hello World!"
+print "Hello World!!\n"
+
+puts "Hello World!"
+puts "Hello World!!"
+
+# コメント
+=begin
+コメント
+コメント
+
+ko
+me
+n
+to
+
+
+=end
+
+p "Hello World!"
+p "Hello World!!"
