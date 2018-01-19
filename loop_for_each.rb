3f8873781ce9240ffd26fc24bf0870b9dcc3f429
森角　拓紀 <morikaku_h@hirokimorikaku.local>
2018-01-19 14:48:56 +0900

    each文で1~100までを表示する繰り返しプログラム

--- loop_for_each.rb
+++ loop_for_each.rb
@@ -3,4 +3,15 @@ puts "for文で1~100までの繰り返し処理開始"
 for num in 1..100 do
   puts num
 end
-puts "for文で1〜100までの繰り返し処理終了"
+puts "for文で1~100までの繰り返し処理終了"
+
+# 改行
+puts
+
+# each文で1-100を表示
+puts "each文で1~100までの繰り返し処理開始"
+range = 1..100
+range.each do |num|
+  puts num
+end
+puts "each文で1~100までの繰り返し処理終了"
