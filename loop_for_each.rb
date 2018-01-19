5c2f6c5f442e48ca86a4e66732aeaca3183e3b12
森角　拓紀 <morikaku_h@hirokimorikaku.local>
2018-01-19 15:14:24 +0900

    for文とeach文を1行での書き方に変更

--- loop_for_each.rb
+++ loop_for_each.rb
@@ -1,8 +1,6 @@
 # for文で1-100を表示
 puts "for文で1~100までの繰り返し処理開始"
-for num in 1..100 do
-  puts num
-end
+for num in 1..100 do puts num end
 puts "for文で1~100までの繰り返し処理終了"
 
 # 改行
@@ -11,7 +9,5 @@ puts
 # each文で1-100を表示
 puts "each文で1~100までの繰り返し処理開始"
 range = 1..100
-range.each do |num|
-  puts num
-end
+range.each do |num| puts num end
 puts "each文で1~100までの繰り返し処理終了"
