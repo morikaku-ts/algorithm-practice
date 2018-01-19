a34f969de7bc26e03b3b3fab4155c12151600054
森角　拓紀 <morikaku_h@hirokimorikaku.local>
2018-01-19 23:11:00 +0900

    不要な改行と改行コメントを削除、メソッド名と仮引数名を変更

--- method.rb
+++ method.rb
@@ -5,16 +5,13 @@ end
 
 add(2, 3)
 
-# 改行
-puts
-
 # 奇数か偶数かを判定
-def num(x)
-  if x %2 == 0
+def odd_or_even(num)
+  if num %2 == 0
     puts "偶数です"
   else
     puts "奇数です"
   end
 end
 
-num(2)
+odd_or_even(2)
