b53899e1f9b49831245cafaf07c115f494909f44
森角　拓紀 <morikaku_h@hirokimorikaku.local>
2018-01-19 18:38:43 +0900

    読みやすさ向上のため、do-end記述部への改行の追加及び{ }記述部への半角スペースの追加

--- loop_for_each.rb
+++ loop_for_each.rb
@@ -1,6 +1,8 @@
 # for文で1~100を表示
 puts "for文で1~100までの繰り返し処理開始"
-for num in 1..100 do puts num end
+for num in 1..100 do
+  puts num
+end
 puts "for文で1~100までの繰り返し処理終了"
 
 # 改行
@@ -9,7 +11,9 @@ puts
 # for文で1~100までの合計を算出
 puts "for文で1~100までの合計を算出"
 sum = 0
-for num in 1..100 do sum = sum + num end
+for num in 1..100 do
+  sum = sum + num
+end
 puts sum
 
 # 改行
@@ -18,7 +22,7 @@ puts
 # each文で1~100を表示
 puts "each文で1~100までの繰り返し処理開始"
 range = 1..100
-range.each do |num| puts num end
+range.each { |num| puts num }
 puts "each文で1~100までの繰り返し処理終了"
 
 # 改行
@@ -28,5 +32,5 @@ puts
 puts "each文で1~100までの合計を算出"
 range = 1..100
 sum = 0
-range.each{|num| sum = sum + num}
+range.each { |num| sum = sum + num }
 puts sum
