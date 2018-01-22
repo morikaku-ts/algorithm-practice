d12a6db1f2a24ebe9016e8dcdf4d2a28a3a14d1e
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-01-22 14:30:25 +0900

    奇数か偶数かの判定及び奇数（2倍）、偶数（半分）の計算する記述の定義を変更。

--- method.rb
+++ method.rb
@@ -1,31 +1,43 @@
-# 和を求める
+# 和を求める定義
 def add(a, b)
-  puts a + b;
+  puts a + b
 end
 
 add(2, 3)
 
+puts
+
 # 奇数か偶数かを判定
-def odd_or_even(num)
-  if num %2 == 0
-    puts "#{num} は偶数です"
+def odd_even(num)
+  if num % 2 == 0
+    puts "#{num}は偶数です。"
   else
-    puts "#{num} は奇数です"
+    puts "#{num}は奇数です。"
   end
 end
 
-for i in 1..100 do
-  odd_or_even(i)
+def odd_or_even(num)
+  odd_even(num)
 end
 
-def odd_or_even(num)
-  if num %2 == 0
-    puts "#{num} の1/2は#{num/2}です"
+for num in 1..100
+  odd_or_even(num)
+end
+
+puts
+
+def odd_twice_even_half(num)
+  if num % 2 == 0
+    puts "#{num}の1/2は#{num/2}です"
   else
-    puts "#{num} の2倍は#{num*2}です"
+    puts "#{num}の2倍は#{num*2}です"
   end
 end
 
-for i in 1..10 do
-  odd_or_even(i)
+def odd_twoce_or_even_half(num)
+  odd_twice_even_half(num)
+end
+
+for num in 1..100
+  odd_twoce_or_even_half(num)
 end
