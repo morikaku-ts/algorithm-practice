6250ae5a6bcb269720ae5725195c75b5c2f77856
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-01-22 15:31:24 +0900

    メソッド名をodd_evenからeven_number?へ変更

--- method.rb
+++ method.rb
@@ -8,13 +8,12 @@ add(2, 3)
 puts
 
 # 奇数か偶数かを判定
-
-def odd_even(num)
+def even_number?(num)
   num % 2 == 0
 end
 
 def odd_or_even(num)
-  if odd_even(num)
+  if even_number?(num)
     puts "#{num}は偶数です。"
   else
     puts "#{num}は奇数です。"
@@ -28,7 +27,7 @@ end
 puts
 
 def odd_twice_or_even_half(num)
-  if odd_even(num)
+  if even_number?(num)
     puts "#{num}の1/2は#{num/2}です"
   else
     puts "#{num}の2倍は#{num*2}です"
