f315cdd080ab24c85f021b48c9e93d8022c52110
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-01-22 15:01:56 +0900

    他のパターンにも応用できるよう、偶数の判定をする数式を再定義

--- method.rb
+++ method.rb
@@ -8,16 +8,17 @@ add(2, 3)
 puts
 
 # 奇数か偶数かを判定
+
 def odd_even(num)
-  if num % 2 == 0
+  num % 2 == 0
+end
+
+def odd_or_even(num)
+  if odd_even(num)
     puts "#{num}は偶数です。"
   else
     puts "#{num}は奇数です。"
   end
-end
-
-def odd_or_even(num)
-  odd_even(num)
 end
 
 for num in 1..100
@@ -26,18 +27,14 @@ end
 
 puts
 
-def odd_twice_even_half(num)
-  if num % 2 == 0
+def odd_twice_or_even_half(num)
+  if odd_even(num)
     puts "#{num}の1/2は#{num/2}です"
   else
     puts "#{num}の2倍は#{num*2}です"
   end
 end
 
-def odd_twoce_or_even_half(num)
-  odd_twice_even_half(num)
-end
-
 for num in 1..100
-  odd_twoce_or_even_half(num)
+  odd_twice_or_even_half(num)
 end
