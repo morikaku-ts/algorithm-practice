669a58085faaa2a284c80eb9286c279ea6cd2f26
森角　拓紀 <morikaku_h@hirokimorikaku.local>
2018-01-30 01:11:49 +0900

    配列を降順に出力するバブルソートプログラムの追加

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,4 @@
+puts "配列を昇順に並び替え"
 def bubble_sort(numbers)
   elements = numbers.length
   repeat_times = elements - 1
@@ -8,6 +9,30 @@ def bubble_sort(numbers)
   repeat_times.times do
     for index in 0..repeat_times - 1
       if numbers[index] > numbers[index + 1]
+        numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
+      end
+    end
+    p numbers
+  end
+  puts "---------------"
+  puts "最終配列結果"
+  p numbers
+end
+bubble_sort([48, 53, 32, 21, 98, 6000, 54, 61, 70, 23, 2, 9, 7])
+
+puts
+
+puts "配列を皇潤に並び替え"
+def bubble_sort(numbers)
+  elements = numbers.length
+  repeat_times = elements - 1
+
+  puts "最初の配列"
+  p numbers
+  puts "---------------"
+  repeat_times.times do
+    for index in 0..repeat_times - 1
+      if numbers[index] < numbers[index + 1]
         numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
       end
     end
