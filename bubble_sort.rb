944aef313adbcf9cd33e4f373efff4e143e03f8f
森角　拓紀 <morikaku_h@hirokimorikaku.local>
2018-01-30 00:53:45 +0900

    配列を昇順に出力するバブルソートプログラム

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(numbers)
+  elements = numbers.length
+  repeat_times = elements - 1
+
+  puts "最初の配列"
+  p numbers
+  puts "---------------"
+  repeat_times.times do
+    for index in 0..repeat_times - 1
+      if numbers[index] > numbers[index + 1]
+        numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
+      end
+    end
+    p numbers
+  end
+  puts "---------------"
+  puts "最終配列結果"
+  p numbers
+end
+bubble_sort([48, 53, 32, 21, 98, 6000, 54, 61, 70, 23, 2, 9, 7])
